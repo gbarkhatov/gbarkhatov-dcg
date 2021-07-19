0b40b6dbc82456071890b11a76e5b0b8d1eba78c
Govard Barkhatov <gbarkhatov@gmail.com>
2021-07-19 16:00:56 +1000

    initial + reformat

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from 'react'
 import {
   ChakraProvider,
   Box,
@@ -7,28 +7,28 @@ import {
   VStack,
   Code,
   Grid,
-  theme,
-} from '@chakra-ui/react';
-import { ColorModeSwitcher } from './ColorModeSwitcher';
-import { Logo } from './Logo';
+  theme
+} from '@chakra-ui/react'
+import { ColorModeSwitcher } from './ColorModeSwitcher'
+import { Logo } from './Logo'
 
-function App() {
+function App () {
   return (
     <ChakraProvider theme={theme}>
-      <Box textAlign="center" fontSize="xl">
-        <Grid minH="100vh" p={3}>
-          <ColorModeSwitcher justifySelf="flex-end" />
+      <Box textAlign='center' fontSize='xl'>
+        <Grid minH='100vh' p={3}>
+          <ColorModeSwitcher justifySelf='flex-end' />
           <VStack spacing={8}>
-            <Logo h="40vmin" pointerEvents="none" />
+            <Logo h='40vmin' pointerEvents='none' />
             <Text>
-              Edit <Code fontSize="xl">src/App.js</Code> and save to reload.
+              Edit <Code fontSize='xl'>src/App.js</Code> and save to reload.
             </Text>
             <Link
-              color="teal.500"
-              href="https://chakra-ui.com"
-              fontSize="2xl"
-              target="_blank"
-              rel="noopener noreferrer"
+              color='teal.500'
+              href='https://chakra-ui.com'
+              fontSize='2xl'
+              target='_blank'
+              rel='noopener noreferrer'
             >
               Learn Chakra
             </Link>
@@ -36,7 +36,7 @@ function App() {
         </Grid>
       </Box>
     </ChakraProvider>
-  );
+  )
 }
 
-export default App;
+export default App
